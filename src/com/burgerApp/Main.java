abfc0bcd1364e09de2ca1eedebb6e8d1c64aa1fd
martgo <martgo@op.pl>
2020-10-19 16:00:12 +0200

    added Main class

--- src/com/burgerApp/Main.java
+++ src/com/burgerApp/Main.java
@@ -2,24 +2,23 @@ package com.burgerApp;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.channels.NotYetBoundException;
+import java.security.PrivateKey;
 
 public class Main {
-    public Main(){
+    public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        JPanel menuSide = new MenuSide();
+        JLabel logo = new JLabel();
         frame.setPreferredSize(new Dimension(500, 500));
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
-        panel.setLayout(new GridLayout(0,1));
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(menuSide);
         frame.setTitle("Burger App");
         frame.setLocation(50, 50);
         frame.pack();
         frame.setVisible(true);
-        ImageIcon burgerAppIcon = new ImageIcon("/src/resources/BurgerAppIcon.png");
-        frame.setIconImage(burgerAppIcon.getImage());
-    }
-    public static void main(String[] args) {
-        new Main();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
