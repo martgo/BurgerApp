d1358ce4e753c5e175221828a1948ef2fe0cabe5
martgo <martgo@op.pl>
2020-10-19 12:09:32 +0200

    added Main class

--- src/com/burgerApp/Main.java
+++ src/com/burgerApp/Main.java
@@ -1,9 +1,23 @@
 package com.burgerApp;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
+    public Main(){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        frame.setPreferredSize(new Dimension(500, 500));
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel.setLayout(new GridLayout(0,1));
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Burger App");
+        frame.setLocation(50, 50);
+        frame.pack();
+        frame.setVisible(true);
+    }
     public static void main(String[] args) {
-        JFrame window = new JFrame("BURGER APP");
+        new Main();
     }
 }
